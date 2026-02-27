95eb4e3e0f1bc50aa768bb436b48235498ba2287
Her Name <hergithubemail@example.com>
2026-02-27 11:28:01 -0700

    add the requiremnts for git clone to pull the file properly

---
+++ risk_app/requirements.R
@@ -0,0 +1,7 @@
+p <- c("tidyquant", "plotly", "TTR", "tidyverse",
+       "dplyr", "magrittr", "lubridate", "tibble",
+       "RTL", "splines", "shiny", "bslib")
+new.packages <- p[!(p %in% installed.packages()[, "Package"])]
+if (length(new.packages)) {
+  install.packages(new.packages, dependencies = TRUE)
+}

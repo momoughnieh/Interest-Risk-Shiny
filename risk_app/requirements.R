e3ce91ebb2859f12e1770002246629f487a8984a
Her Name <hergithubemail@example.com>
2026-02-27 11:35:34 -0700

    forgot to add string R to requirements which didnt let the app actually load form image

--- risk_app/requirements.R
+++ risk_app/requirements.R
@@ -1,6 +1,6 @@
 p <- c("tidyquant", "plotly", "TTR", "tidyverse",
        "dplyr", "magrittr", "lubridate", "tibble",
-       "RTL", "splines", "shiny", "bslib")
+       "RTL", "splines", "shiny", "bslib", "stringr")
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
   install.packages(new.packages, dependencies = TRUE)

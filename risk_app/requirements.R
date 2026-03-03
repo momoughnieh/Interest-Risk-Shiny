f7e2174d30b8fe86dd18b83a585895dae43fd92f
Cai Naidoo <cnaidoo@ualberta.ca>
2026-03-02 19:19:32 -0700

    factoextra

--- risk_app/requirements.R
+++ risk_app/requirements.R
@@ -1,6 +1,6 @@
 p <- c("tidyquant", "plotly", "TTR", "tidyverse",
        "dplyr", "magrittr", "lubridate", "tibble",
-       "RTL", "splines", "shiny", "bslib", "ggcorrplot")
+       "RTL", "splines", "shiny", "bslib", "ggcorrplot", "factoextra")
 new.packages <- p[!(p %in% installed.packages()[, "Package"])]
 if (length(new.packages)) {
   install.packages(new.packages, dependencies = TRUE)
